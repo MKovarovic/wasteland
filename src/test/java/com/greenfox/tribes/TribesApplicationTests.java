20bd96c8d7e50c254724369ce0d162a7885bfb1a
Matěj Frnka <mat.frnka@gmail.com>
2023-12-04 09:29:22 +0100

    init

---
+++ src/test/java/com/greenfox/tribes/TribesApplicationTests.java
@@ -0,0 +1,11 @@
+package com.greenfox.tribes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TribesApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
