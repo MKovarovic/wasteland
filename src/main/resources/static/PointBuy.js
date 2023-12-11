f3e1d437a7eb31fb3f47e4bed41d1cee3740a3f7
Matous <matous.kovarovic@seznam.cz>
2023-12-11 14:38:41 +0100

    found better point buy code, now it works

--- src/main/resources/static/PointBuy.js
+++ src/main/resources/static/PointBuy.js
@@ -1,85 +1,12 @@
-
-
-let pointsToSpend;
-pointsToSpend = 100;
-maxStat = 50;
-pointsMax = pointsToSpend;
-
-jQuery(document).ready(function () {
-    $('.pointsToSpend').append(pointsToSpend);
-    $('.pointsMax').append(pointsMax);
-    $('.qtyplus').click(function (e) {
-        e.preventDefault();
-        fieldName = $(this).attr('field');
-        costName = fieldName + "-cost";
-        var currentVal = parseInt($('input[name=' + fieldName + ']').val());
-        var costVal = parseInt($('input[name=' + costName + ']').val());
-        if (currentVal !== maxStat) {
-            if (pointsToSpend === 0) {
-                $('input[name=' + fieldName + ']').val(currentVal + 0);
-                $('input[name=' + costName + ']').val(costVal + 0);
-            } else if (pointsToSpend === 1) {
-                if (currentVal >= 14) {
-                } else {
-                    $('input[name=' + fieldName + ']').val(currentVal + 1);
-                    $('input[name=' + costName + ']').val(costVal + 1);
-                    pointsToSpend -= 1;
-                }
-            } else if (!isNaN(currentVal)) {
-                currentVal = currentVal + 1;
-                if (currentVal === 14) {
-                    pointsToSpend -= 2;
-                    costVal = costVal + 2;
-                } else if (currentVal === 15) {
-                    pointsToSpend -= 2;
-                    costVal = costVal + 2;
-                } else {
-                    pointsToSpend -= 1;
-                    costVal = costVal + 1;
-                }
-                $('input[name=' + fieldName + ']').val(currentVal);
-                $('input[name=' + costName + ']').val(costVal);
-            } else {
-                $('input[name=' + fieldName + ']').val(0);
-                $('input[name=' + costName + ']').val(0);
-            }
-        }
-        $('.pointsToSpend').text("");
-        $('.pointsToSpend').append(pointsToSpend);
-    });
-    $(".qtyminus").click(function (e) {
-        e.preventDefault();
-        fieldName = $(this).attr('field');
-        costName = fieldName + "-cost";
-        var currentVal = parseInt($('input[name=' + fieldName + ']').val());
-        var costVal = parseInt($('input[name=' + costName + ']').val());
-        if (!isNaN(currentVal) && currentVal > 8) {
-            if (currentVal === 14) {
-                pointsToSpend += 2;
-                costVal -= 2;
-            } else if (currentVal === 15) {
-                pointsToSpend += 2;
-                costVal -= 2;
-            } else {
-                pointsToSpend += 1;
-                costVal -= 1;
-            }
-            $('input[name=' + fieldName + ']').val(currentVal - 1);
-            $('input[name=' + costName + ']').val(costVal);
-        } else {
-            $('input[name=' + fieldName + ']').val(8);
-            $('input[name=' + costName + ']').val(0);
-        }
-
-        $('.pointsToSpend').text("");
-        $('.pointsToSpend').append(pointsToSpend);
-    });
-    $('.qtyplus,.qtyminus').click(function (e) {
-        fieldName = $(this).attr('field');
-        modName = fieldName + "-modifier";
-        scoreName = fieldName + "-score";
-        var currentVal = parseInt($('input[name=' + fieldName + ']').val());
-
-
-    });
-});
+function get(id){
+    return Number(document.getElementById(id).value)
+}
+function set(id,value){
+    document.getElementById(id).value=value;
+}
+function recalculate(){
+    let total=0;
+    for(const input of document.querySelectorAll(".abilityBuy"))total+=Number(input.value)-Number(input.min);
+    set("points",100-total);
+}
+recalculate();
