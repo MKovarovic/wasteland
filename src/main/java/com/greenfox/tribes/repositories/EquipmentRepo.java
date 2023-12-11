f93d5b1e8a332c6476ad3090ca60fed8e251d317
Matous <matous.kovarovic@seznam.cz>
2023-12-11 12:04:20 +0100

    added repositories for character and for equipment

---
+++ src/main/java/com/greenfox/tribes/repositories/EquipmentRepo.java
@@ -0,0 +1,10 @@
+package com.greenfox.tribes.repositories;
+
+
+import com.greenfox.tribes.models.Equipment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EquipmentRepo extends JpaRepository<Equipment, Long> {
+}
