2bf82df40c8be93d98f391f6688d10535fc0b257
Matous <matous.kovarovic@seznam.cz>
2023-12-11 16:14:14 +0100

    basics of character screen

--- src/main/java/com/greenfox/tribes/controllers/UserInterface.java
+++ src/main/java/com/greenfox/tribes/controllers/UserInterface.java
@@ -1,13 +1,28 @@
 package com.greenfox.tribes.controllers;
 
+import com.greenfox.tribes.models.dtos.CharacterDTO;
+import com.greenfox.tribes.services.CharacterService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 
 @Controller
 public class UserInterface {
 
+    @Autowired
+    CharacterService characterService;
+
     @GetMapping("/character/new")
-    public static String newCharacter(){
+    public String newCharacter(){
         return "characterCreation";
+    }
+
+    @GetMapping("/character/me")
+    public  String myCharacter(Model model, @RequestAttribute("id") Long id){
+        CharacterDTO dto = characterService.readCharacter(id);
+        model.addAttribute("DTO", dto);
+        return "MainPage";
     }
 }
