202b7682b48b82c0d24efcb78b1d2d3f50e641e1
Matous <matous.kovarovic@seznam.cz>
2024-01-15 17:16:28 +0100

    WB-25: Portrait Repository added

--- src/main/java/com/greenfox/tribes/services/PortraitService.java
+++ src/main/java/com/greenfox/tribes/services/PortraitService.java
@@ -2,6 +2,7 @@ package com.greenfox.tribes.services;
 
 import com.greenfox.tribes.models.Portrait;
 import com.greenfox.tribes.repositories.PersonaRepository;
+import com.greenfox.tribes.repositories.PortraitRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,9 @@ public class PortraitService {
     @Autowired
     PersonaRepository personaRepository;
 
+    @Autowired
+    PortraitRepository portraitRepository;
+
     public void createPortrait(String head, String hair, String eyes, String nose, String mouth, Long personId) {
         Portrait portrait = new Portrait();
         portrait.setHead(head);
@@ -19,6 +23,7 @@ public class PortraitService {
         portrait.setNose(nose);
         portrait.setMouth(mouth);
         portrait.setPersona(personaRepository.findById(personId).orElse(null));
+        portraitRepository.save(portrait);
 
     }
 
