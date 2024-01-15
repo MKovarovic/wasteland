3f75aac20f66f12e976ca898c8806367c053cde5
Matous <matous.kovarovic@seznam.cz>
2024-01-15 17:20:53 +0100

    WB-25: Portrait DTO added

---
+++ src/main/java/com/greenfox/tribes/dtos/PortraitDTO.java
@@ -0,0 +1,12 @@
+package com.greenfox.tribes.dtos;
+
+import lombok.Data;
+
+@Data
+public class PortraitDTO {
+    private Long personaId;
+    private String head;
+    private String hair;
+    private String eyes;
+    private String nose;
+    private String mouth;}
--- src/main/java/com/greenfox/tribes/services/PortraitService.java
+++ src/main/java/com/greenfox/tribes/services/PortraitService.java
@@ -1,5 +1,6 @@
 package com.greenfox.tribes.services;
 
+import com.greenfox.tribes.dtos.PortraitDTO;
 import com.greenfox.tribes.models.Portrait;
 import com.greenfox.tribes.repositories.PersonaRepository;
 import com.greenfox.tribes.repositories.PortraitRepository;
@@ -9,22 +10,31 @@ import org.springframework.stereotype.Service;
 @Service
 public class PortraitService {
 
-    @Autowired
-    PersonaRepository personaRepository;
+  @Autowired PersonaRepository personaRepository;
 
-    @Autowired
-    PortraitRepository portraitRepository;
+  @Autowired PortraitRepository portraitRepository;
 
-    public void createPortrait(String head, String hair, String eyes, String nose, String mouth, Long personId) {
-        Portrait portrait = new Portrait();
-        portrait.setHead(head);
-        portrait.setHair(hair);
-        portrait.setEyes(eyes);
-        portrait.setNose(nose);
-        portrait.setMouth(mouth);
-        portrait.setPersona(personaRepository.findById(personId).orElse(null));
-        portraitRepository.save(portrait);
+  public void createPortrait(
+      String head, String hair, String eyes, String nose, String mouth, Long personId) {
+    Portrait portrait = new Portrait();
+    portrait.setHead(head);
+    portrait.setHair(hair);
+    portrait.setEyes(eyes);
+    portrait.setNose(nose);
+    portrait.setMouth(mouth);
+    portrait.setPersona(personaRepository.findById(personId).orElse(null));
+    portraitRepository.save(portrait);
+  }
 
-    }
-
+  public PortraitDTO findPortrait(long id) {
+    Portrait portrait = portraitRepository.findById(id).get();
+    PortraitDTO dto = new PortraitDTO();
+    dto.setPersonaId(portrait.getPersona().getId());
+    dto.setHead(portrait.getHead());
+    dto.setHair(portrait.getHair());
+    dto.setEyes(portrait.getEyes());
+    dto.setNose(portrait.getNose());
+    dto.setMouth(portrait.getMouth());
+    return dto;
+  }
 }
