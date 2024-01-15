65b1b41514d9be9f9d094b326e3e32501ac49f6e
Matous <matous.kovarovic@seznam.cz>
2024-01-15 16:52:50 +0100

    WB-25: Face Parts added, face model added as well

---
+++ src/main/java/com/greenfox/tribes/models/Portrait.java
@@ -0,0 +1,12 @@
+package com.greenfox.tribes.models;
+
+public class Portrait {
+    private Long id;
+    private int head;
+    private int hair;
+    private int eyes;
+    private int eyebrows;
+    private int nose;
+    private int mouth;
+
+}
