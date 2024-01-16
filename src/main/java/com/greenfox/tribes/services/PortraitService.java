05fc745ba276f258ad7faf436ca15137427fe7b1
Matous <matous.kovarovic@seznam.cz>
2024-01-16 10:43:39 +0100

    WB-25: CSS and HTML of the overview page modified, structure now accepts portrait

--- src/main/java/com/greenfox/tribes/services/PortraitService.java
+++ src/main/java/com/greenfox/tribes/services/PortraitService.java
@@ -10,31 +10,34 @@ import org.springframework.stereotype.Service;
 @Service
 public class PortraitService {
 
-  @Autowired PersonaRepository personaRepository;
+    @Autowired
+    PersonaRepository personaRepository;
 
-  @Autowired PortraitRepository portraitRepository;
+    @Autowired
+    PortraitRepository portraitRepository;
 
-  public void createPortrait(
-      String head, String hair, String eyes, String nose, String mouth, Long personId) {
-    Portrait portrait = new Portrait();
-    portrait.setHead(head);
-    portrait.setHair(hair);
-    portrait.setEyes(eyes);
-    portrait.setNose(nose);
-    portrait.setMouth(mouth);
-    portrait.setPersona(personaRepository.findById(personId).orElse(null));
-    portraitRepository.save(portrait);
-  }
+    public void createPortrait(String head, String hair, String eyes, String nose, String mouth, Long personId) {
+        Portrait portrait = new Portrait();
+        portrait.setHead(head);
+        portrait.setHair(hair);
+        portrait.setEyes(eyes);
+        portrait.setNose(nose);
+        portrait.setMouth(mouth);
+        portrait.setPersona(personaRepository.findById(personId).orElse(null));
+        portraitRepository.save(portrait);
 
-  public PortraitDTO findPortrait(long id) {
-    Portrait portrait = portraitRepository.findById(id).get();
-    PortraitDTO dto = new PortraitDTO();
+    }
+
+    public PortraitDTO findPortrait(long id) {
+        Portrait portrait = portraitRepository.findById(id).get();
+        PortraitDTO dto = new PortraitDTO();
     dto.setPersonaId(portrait.getPersona().getId());
-    dto.setHead(portrait.getHead());
-    dto.setHair(portrait.getHair());
-    dto.setEyes(portrait.getEyes());
-    dto.setNose(portrait.getNose());
-    dto.setMouth(portrait.getMouth());
-    return dto;
-  }
+        dto.setHead(portrait.getHead());
+        dto.setHair(portrait.getHair());
+        dto.setEyes(portrait.getEyes());
+        dto.setNose(portrait.getNose());
+        dto.setMouth(portrait.getMouth());
+        return dto;
+    }
+
 }
