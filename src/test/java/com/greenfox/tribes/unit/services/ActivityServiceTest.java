e9c519793454f6a82665190fda855c8006f7292d
Matous <matous.kovarovic@seznam.cz>
2024-01-31 16:03:48 +0100

    WB-36: LogPVP test added

--- src/test/java/com/greenfox/tribes/unit/services/ActivityServiceTest.java
+++ src/test/java/com/greenfox/tribes/unit/services/ActivityServiceTest.java
@@ -12,42 +12,63 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class ActivityServiceTest extends BaseTest {
 
-  @Mock
-  private PersonaService personaService;
+  @Mock private PersonaService personaService;
   @Mock private ActivityLogRepository activityRepository;
-  @InjectMocks
-  private ActivityService activityService;
+  @InjectMocks private ActivityService activityService;
 
   @Test
   public void ActivityService_logWork_logNotEmpty() {
-    Persona persona = createTestPersona();
-
+    Persona persona = createTestRaider();
 
     when(personaService.getLoggedInPersona()).thenReturn(persona);
     activityService.logWorkActivity();
 
     ArgumentCaptor<ActivityLog> argument = ArgumentCaptor.forClass(ActivityLog.class);
-;
-
+    ;
 
     verify(activityRepository).save(argument.capture());
     assertEquals(persona, argument.getValue().getPersona());
     assertEquals(ActivityType.WORK, argument.getValue().getType());
-
   }
 
-  private Persona createTestPersona() {
+  @Test
+  public void ActivityService_logPvP_logNotEmpty() {
+    Persona persona = createTestRaider();
+    ActivityLog activityLog = new ActivityLog();
+    activityLog.setPersona(persona);
+    activityLog.setType(ActivityType.PVP);
+    activityLog.setId(1L);
+    activityLog.setGivesItem(false);
+    activityLog.setPullRings(100);
+    activityLog.setTime(5);
+    activityLog.setTimestamp(System.currentTimeMillis());
+
+    when(personaService.getLoggedInPersona()).thenReturn(persona);
+    when(activityRepository.findActivityLogByPersonaId(1L)).thenReturn(Optional.of(activityLog));
+    activityService.logPVPActivity(1L);
+
+    ArgumentCaptor<ActivityLog> argument = ArgumentCaptor.forClass(ActivityLog.class);
+    verify(activityRepository, times(2)).save(argument.capture());
+    assertEquals(persona, argument.getValue().getPersona());
+    assertEquals(ActivityType.PVP, argument.getValue().getType());
+  }
+
+  private Persona createTestRaider() {
     Persona persona = new Persona("JoeMama", Faction.RAIDER, 50, 20, 10, 10, 100, 1);
+    persona.setId(1L);
+    return persona;
+  }
+
+  private Persona createTestSettler() {
+    Persona persona = new Persona("JoeMama", Faction.SETTLER, 50, 20, 10, 10, 100, 1);
     persona.setId(1L);
     return persona;
   }
